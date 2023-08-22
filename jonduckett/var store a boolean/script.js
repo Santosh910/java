2c3f494bd6169f1683e129ec88bdfacce57c9878
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-08-22 23:15:15 +0530

    update

---
+++ jonduckett/var store a boolean/script.js
@@ -0,0 +1,9 @@
+var inStock= true,shipping= false;
+
+inStock= false ;
+shipping ;
+
+var elStock = document.getElementById('stock');
+elStock.textContent = inStock;
+var elship = document.getElementById('shipping');
+elship.textContent = shipping;
